16e5aa984681567c1af6642b3129a0080b30ced5
Bruno Marques <anderson.bruno90@gmail.com>
2025-08-15 12:24:28 -0300

    adiciona modal com detalhes dos clients

--- src/app/modules/apps/client-management/clients-list/ClientList.tsx
+++ src/app/modules/apps/client-management/clients-list/ClientList.tsx
@@ -5,13 +5,9 @@ import { useMutation, useQuery, UseQueryResult } from 'react-query'
 import { Column } from 'react-table'
 import { deleteClient, getList } from './core/_requests'
 import { PaginatedResponse, usePagination } from '@contexts/PaginationContext'
+import { useState } from 'react'
+import { ClientDetailsModal } from '@components/list-view/components/modals/ClientDetailsModal'
 
-const columnsFake: Column<ClientType>[] = [
-  { Header: 'Nome', accessor: 'name' },
-  { Header: 'Total de contas', accessor: 'total_accounts' },
-  { Header: 'Contas restantes', accessor: 'remaining_accounts' },
-  { Header: 'Parceiro', accessor: 'partner' },
-]
 
 const clients: ClientType[] = [
   { name: 'Cliente - Escola 1', total_accounts: 10, remaining_accounts: 3, partner: 'Parceiro 1' },
@@ -28,14 +24,61 @@ const ClientListWrapper = () => {
       keepPreviousData: true,
     }
   )
+  
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  
+  const [selectedClientId, setSelectedClientId] = useState<string | null>(null)
+  
+  const handleOpenModal = (cLientId: string) => {
+    setSelectedClientId(cLientId)
+    setIsModalOpen(true)
+  }
+  
+  const handleCloseModal = () => {
+    console.log("A função handleCloseModal FOI CHAMADA!");
+    setSelectedClientId(null)
+    setIsModalOpen(false)
+  }
+  
+  const columnsFake: Column<ClientType>[] = [
+  { 
+    Header: 'Nome', 
+    accessor: 'name',
+    Cell: ({ row }) => (
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          if (row.original.id) {
+            handleOpenModal(row.original.id);
+          }
+        }}
+        className='text-primary fw-bold'
+      >
+        {row.original.name}
+      </a>
+    )
+  },
+    { Header: 'Total de contas', accessor: 'total_accounts' },
+    { Header: 'Contas restantes', accessor: 'remaining_accounts' },
+    { Header: 'Parceiro', accessor: 'partner' },
+  ]
 
   return (
+    <>
     <ListView 
-      data={data?.items || []}
+    data={data?.items || []}
       columns={columnsFake}
       isLoading={isLoading}
       totalItems={data?.totalCount || 0}
     />
+     {isModalOpen && (
+        <ClientDetailsModal
+          clientId={selectedClientId!} 
+          onClose={handleCloseModal}
+        />
+      )}
+    </>
   )
 }
 
